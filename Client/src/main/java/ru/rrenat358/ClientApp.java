daace7c8ad971a3c006892347112629ec8c5047a
rrenat358 <rrenat358@gmail.com>
2022-10-03 14:40:06 +0300

    1440

--- Client/src/main/java/ru/rrenat358/ClientApp.java
+++ Client/src/main/java/ru/rrenat358/ClientApp.java
@@ -2,8 +2,11 @@ package ru.rrenat358;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.Getter;
+import ru.rrenat358.controllers.MainWindow;
 
 public class ClientApp extends Application {
 
@@ -13,6 +16,15 @@ public class ClientApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         this.clientStage = stage;
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(MainWindow.class.getResource("mainWindow.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        clientStage.setScene(scene);
+        clientStage.show();
+
+
 
     }
 
--- Client/src/main/java/ru/rrenat358/controllers/MainWindow.java
+++ Client/src/main/java/ru/rrenat358/controllers/MainWindow.java
@@ -6,8 +6,10 @@ package ru.rrenat358.controllers;
 
 public class MainWindow {
 
+/*
     @FXML
     public Button sendButton;
+*/
 
 
 }
