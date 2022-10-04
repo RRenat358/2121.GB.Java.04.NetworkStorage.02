aa666b2cab9ef46b89468bcd907ec1347552f106
rrenat358 <rrenat358@gmail.com>
2022-10-04 11:32:39 +0300

    1132

---
+++ Common/src/main/java/ru.rrenat358/ConfigConst.java
@@ -0,0 +1,27 @@
+package ru.rrenat358;
+
+
+
+public class ConfigConst {
+
+    //Common
+    public static final String HOST = "localhost";
+
+    public static final int PORT = 68461;
+
+
+
+    //Client
+    public static String CLIENT_REPO = "Client/DataUser/";
+
+
+
+
+    //Server
+    public static String SERVER_REPO = "Client/DataUser/";
+
+    public static final int MAXIMUM_OBJECT_SIZE = 1024 * 1024 * 10;
+
+
+
+}
