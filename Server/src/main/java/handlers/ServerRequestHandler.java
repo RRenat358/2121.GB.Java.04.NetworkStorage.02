86b060af3dca060fda0e7f097c363ef6987c0cfa
rrenat358 <rrenat358@gmail.com>
2022-10-27 17:59:06 +0300

    1759

---
+++ Server/src/main/java/handlers/ServerRequestHandler.java
@@ -0,0 +1,10 @@
+package handlers;
+
+import io.netty.channel.ChannelHandlerContext;
+import ru.geekbrains.cloud.server.service.ClientService;
+
+public interface ServerRequestHandler {
+
+  void handle(ChannelHandlerContext ctx, Object msg, ClientService clientService);
+
+}
