a783e42316e92ff43bd7aa45b90b5485630a0a43
rrenat358 <rrenat358@gmail.com>
2022-10-31 18:15:04 +0300

    1815

---
+++ Client/src/main/java/ru/rrenat358/handlers/ListResponseHandler.java
@@ -0,0 +1,29 @@
+package ru.rrenat358.handlers;
+
+import io.netty.channel.ChannelHandlerContext;
+import javafx.application.Platform;
+import lombok.extern.log4j.Log4j2;
+import ru.geekbrains.cloud.client.javafx.Controller;
+import ru.geekbrains.cloud.common.messages.list.FileInfo;
+import ru.geekbrains.cloud.common.messages.list.ListResponse;
+
+import java.util.List;
+
+@Log4j2
+public class ListResponseHandler implements ClientRequestHandler {
+
+  @Override
+  public void handle(ChannelHandlerContext ctx, Object msg, Controller controller) {
+    ListResponse listResponse = (ListResponse) msg;
+
+    List<FileInfo> list = listResponse.getList();
+    String path = listResponse.getPath();
+
+    log.info("Received a list from the server: " + path);
+
+    Platform.runLater(() -> {
+      controller.updateList(list);
+      controller.updatePathField(path);
+    });
+  }
+}
