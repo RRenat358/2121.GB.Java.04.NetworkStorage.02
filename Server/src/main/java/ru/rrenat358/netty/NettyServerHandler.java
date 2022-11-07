20161fbab1db597c4967fa90d95b832c9c328b61
rrenat358 <rrenat358@gmail.com>
2022-11-07 19:34:37 +0300

    1934

---
+++ Server/src/main/java/ru/rrenat358/netty/NettyServerHandler.java
@@ -0,0 +1,31 @@
+package ru.rrenat358.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.log4j.Log4j2;
+import ru.geekbrains.cloud.server.handlers.ServerHandlerRegistry;
+import ru.geekbrains.cloud.server.handlers.ServerRequestHandler;
+import ru.geekbrains.cloud.server.service.ClientService;
+
+@Log4j2
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+  private final ClientService clientService = new ClientService();
+
+  @Override
+  public void channelActive(ChannelHandlerContext ctx) {
+    log.info("Client connected: " + ctx.name());
+  }
+
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    ServerRequestHandler handler = ServerHandlerRegistry.getHandler(msg.getClass());
+    handler.handle(ctx, msg, clientService);
+  }
+
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    cause.printStackTrace();
+    ctx.close();
+  }
+}
