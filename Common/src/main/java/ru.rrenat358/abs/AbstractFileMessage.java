a369cc30f3f98c4e075fa752f6c51a7dea820596
rrenat358 <rrenat358@gmail.com>
2022-11-07 19:26:33 +0300

    1926

---
+++ Common/src/main/java/ru.rrenat358/abs/AbstractFileMessage.java
@@ -0,0 +1,13 @@
+package ru.rrenat358.abs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public abstract class AbstractFileMessage extends AbstractMessage{
+
+  private String fileName;
+  private String path;
+
+}
---
+++ Common/src/main/java/ru.rrenat358/abs/AbstractMessage.java
@@ -0,0 +1,7 @@
+package ru.rrenat358.abs;
+
+import java.io.Serializable;
+
+public abstract class AbstractMessage implements Serializable {
+
+}
